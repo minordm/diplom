540926d51dcb3a961ffff8754553fcb7e5f59f16
minor.dm <lugovoi21@gmail.com>
2025-02-24 14:03:52 +0500

    Добавил svg и уровень нефти, газа, воды

--- src/components/Barrel/Barrel.jsx
+++ src/components/Barrel/Barrel.jsx
@@ -1,19 +1,22 @@
 import "./Barrel.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 function Barrel() {
-  const [gasHeight, setGasHeight] = useState("33%");
-  const [oilHeight, setOilHeight] = useState("33%");
-  const [waterHeight, setWaterHeight] = useState("33%");
+  // const [oilValues, setOilValues] = useState("");
+  // const [gasValues, setGasValues] = useState("");
 
-  useEffect(() => {
-    // Пример изменения высоты полосок
-    setTimeout(() => {
-      setGasHeight("20%");
-      setOilHeight("50%");
-      setWaterHeight("30%");
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   const oilAnimate = document.querySelector("#oil-rect animate");
+  //   const gasAnimate = document.querySelector("#gas-rect animate");
+
+  //   if (oilAnimate) {
+  //     oilAnimate.setAttribute("values", oilValues);
+  //   }
+
+  //   if (gasAnimate) {
+  //     gasAnimate.setAttribute("values", gasValues);
+  //   }
+  // }, [oilValues, gasValues]);
 
   return (
     <div className="barrel">
@@ -25,20 +28,21 @@ function Barrel() {
         <div className="barrel__img">
           <img
             className="barrel__img_svg"
-            src="./barrel.svg"
+            src="./barrel_with.svg"
             alt="barrel pic"
           />
-          <div className="barrel__level">
-            <div className="barrel__level_gas" style={{ height: gasHeight }}></div>
-            <div className="barrel__level_oil" style={{ height: oilHeight }}></div>
-            <div className="barrel__level_water" style={{ height: waterHeight }}></div>
-          </div>
         </div>
 
-        <div className="barrel__level_panel">
+        <div className="barrel__right_panel">
           <div className="barrel__level_num">
             <div className="barrel__level_text">8,34</div>
             <div className="barrel__level_text">8,05</div>
+          </div>
+
+          <div className="barrel__level">
+            <div className="barrel__level_gas"></div>
+            <div className="barrel__level_oil"></div>
+            <div className="barrel__level_water"></div>
           </div>
 
           <div className="barrel__level_name">
@@ -48,6 +52,17 @@ function Barrel() {
           </div>
         </div>
       </div>
+
+      {/* <div>
+        <p>Oil Values: {oilValues}</p>
+        <p>Gas Values: {gasValues}</p>
+        <button onClick={() => setOilValues("100;150;100")}>
+          Change Oil Values
+        </button>
+        <button onClick={() => setGasValues("30;70;30")}>
+          Change Gas Values
+        </button>
+      </div> */}
     </div>
   );
 }
