66099239a1b1b5577a4fcff4384621ec0bbab622
minor.dm <lugovoi21@gmail.com>
2025-02-28 15:45:29 +0500

    изменение svg (таблички)

--- src/layout/Main/BodyScheme/BodyScheme.jsx
+++ src/layout/Main/BodyScheme/BodyScheme.jsx
@@ -1,23 +1,10 @@
 import "./BodyScheme.css";
-
-// function BodyScheme() {
-//   return (
-//     <div className="body-scheme">
-//     </div>
-//   );
-// }
-
-// export default BodyScheme;
-
 import { ReactSVG } from "react-svg";
 
-function BodyScheme() {
+export default function BodyScheme() {
   return (
     <div className="body-scheme">
       <ReactSVG src="./scheme2-01.svg" className='scheme1' />
-      {/* <img src="./scheme.svg" alt="barrel pic" /> */}
     </div>
   );
 }
-
-export default BodyScheme;
